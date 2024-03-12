f61a54542befb149d42c61010e6402bcdd4cf201
Krzysztof Krystian Jankowski <kj@p1x.in>
2024-03-12 22:54:31 +0100

    alpha 8

--- OS/os/os.ino
+++ OS/os/os.ino
@@ -31,13 +31,14 @@ Quick log:
 - [release] alpha 7
 - [done] screensaver
 - [done] dim background under windows
-- 
+- [done] arrows to select icons app
+- [done] enter for running apps from icon selection 
+- [done] fixed root problem for crashing (one global text buffer without free())
 
 ToDo:
-- arrow selection for icons
+- SD card read/write
 - text editor prog
 - piano prog
-- SD card read/write
 - paint prog
 - amiga mouse input
 
@@ -72,6 +73,7 @@ const byte TUNE_POS = 1;
 const byte TUNE_NEG = 2;
 const byte TUNE_NEU = 3;
 const byte WALLPAPERS = 3;
+const byte ICONS = 7;
 
 const char TXT_ICON0[8] PROGMEM = "Free\0";
 const char TXT_ICON1[8] PROGMEM = "About\0";
@@ -79,11 +81,14 @@ const char TXT_ICON2[8] PROGMEM = "Look\0";
 const char TXT_ICON3[8] PROGMEM = "P1X\0";
 const char TXT_ICON4[8] PROGMEM = "Edit\0";
 const char TXT_ICON5[8] PROGMEM = "Files\0";
+const char TXT_ICON6[8] PROGMEM = "Moncia\0";
+const char TXT_ICON7[8] PROGMEM = "SSaver\0";
 const char TXT_BTNCLOSE[8] PROGMEM = "Close\0";
 const char TXT_BTNOK[8] PROGMEM = "OK\0";
 const char TXT_BTNCHANGE[8] PROGMEM = "Change\0";
 const char TXT_UNKNOWN[24] PROGMEM = "Unknown command\0";
 const char TXT_MONCIAABOUT[24] PROGMEM = "MonciaOS: alpha \0";
+const char TXT_DEBUG[24] PROGMEM = "DEBUG\0";
 const char TXT_DONE[24] PROGMEM = "Done\0";
 const char TXT_CC[24] PROGMEM = "CC0 2024 P1X\0";
 const char TXT_FREERAM[24] PROGMEM = "Free RAM: \0";
@@ -101,22 +106,25 @@ const char TXT_JANKOWSKI[24] PROGMEM = "Jankowski\0";
 
 char commandBuffer[CMD_MAX + 1];
 char commandBufferLast[CMD_MAX + 1];
+char txtBuf[24];
+char txtBuf8[8];
 byte cmdIndex = 0;
 byte cmdIndexLast = 0;
+byte iconIndex = 0;
 byte wallpaper = 0;
 unsigned long lastActivityTime = 0;
 const unsigned long screensaverTimeout = 60000;
-byte selectedIcon = 0;
+
 
 static int freeRam();
-static void bootimage();
-static void prompt();
-static void clear_cmd();
-static void drawIcon(const byte pos, const byte id, const bool selected);
-static void draw_desktop();
-static void play_tune(const byte no);
-static void drawWindow(const byte ww, const byte wh, const bool btn = true);
-static void p1x();
+void bootimage();
+void prompt();
+void clear_cmd();
+void drawIcon(const byte pos, const byte id, const bool selected);
+void draw_desktop();
+void play_tune(const byte no);
+void drawWindow(const byte ww, const byte wh, const bool btn = true);
+void p1x();
 void processCommand(char *command);
 
 void setup()  {
@@ -140,25 +148,24 @@ static int freeRam() {
   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
 }
 
-static void bootimage(){
+void bootimage(){
   TV.bitmap(0,0,img_title); 
-  _delay_ms(1500);
+  _delay_ms(500);
 }
 
-static void prompt(){
+void prompt(){
   TV.draw_rect(6+2,74+2,110,12,BLACK,BLACK);
   TV.draw_rect(6,74,110,12,WHITE,BLACK);
   TV.set_cursor(10,78);
   TV.print("> ");
 }
 
-static void clear_cmd(){
+void clear_cmd(){
   cmdIndex = 0;
   memset(commandBuffer, 0, sizeof(commandBuffer));
 }
 
-static void drawIcon(const byte pos, const byte id, const bool selected = false){
-  char txtBuff[8];
+void drawIcon(const byte pos, const byte id, const bool selected = false){
   const byte ix = 8 + (pos%3)*38;
   const byte iy = 4 + (pos/3)*22;
   TV.draw_rect(ix+2,iy+2,32,16,BLACK,BLACK);
@@ -168,20 +175,21 @@ static void drawIcon(const byte pos, const byte id, const bool selected = false)
   TV.set_cursor(ix+2,iy+2);
 
   switch (id) {
-    case 0: strcpy_P(txtBuff,TXT_ICON0); TV.print(txtBuff); break;
-    case 1: strcpy_P(txtBuff,TXT_ICON1); TV.print(txtBuff); break;
-    case 2: strcpy_P(txtBuff,TXT_ICON2); TV.print(txtBuff); break;
-    case 3: strcpy_P(txtBuff,TXT_ICON3); TV.print(txtBuff); break;
-    case 4: strcpy_P(txtBuff,TXT_ICON4); TV.print(txtBuff); break;
-    case 5: strcpy_P(txtBuff,TXT_ICON5); TV.print(txtBuff); break;
-    default: TV.print("Unknow");
+    case 0: strcpy_P(txtBuf8,TXT_ICON0); TV.print(txtBuf8); break;
+    case 1: strcpy_P(txtBuf8,TXT_ICON1); TV.print(txtBuf8); break;
+    case 2: strcpy_P(txtBuf8,TXT_ICON2); TV.print(txtBuf8); break;
+    case 3: strcpy_P(txtBuf8,TXT_ICON3); TV.print(txtBuf8); break;
+    case 4: strcpy_P(txtBuf8,TXT_ICON4); TV.print(txtBuf8); break;
+    case 5: strcpy_P(txtBuf8,TXT_ICON5); TV.print(txtBuf8); break;
+    case 6: strcpy_P(txtBuf8,TXT_ICON6); TV.print(txtBuf8); break;
+    case 7: strcpy_P(txtBuf8,TXT_ICON7); TV.print(txtBuf8); break;
   }
 
 
 }
 
-static void drawWindow(const byte ww, const byte wh, const bool btn = true){
-  char txtBuff[8];
+void drawWindow(const byte ww, const byte wh, const bool btn = true){
+  
   const byte wx = 64 - ww/2;  
   byte wy = 48 - wh/2;
   if (wh>48) { wy = 4; }
@@ -195,16 +203,16 @@ static void drawWindow(const byte ww, const byte wh, const bool btn = true){
   TV.draw_rect(64-18+4,wy+wh+2+4,36,12,BLACK,BLACK);
   TV.draw_rect(64-18,wy+wh+2,36,12,BLACK,WHITE);
   TV.set_cursor(64-10,wy+wh+4+2);
-  strcpy_P(txtBuff, TXT_BTNCLOSE);
-  TV.print(txtBuff);
+  strcpy_P(txtBuf8, TXT_BTNCLOSE);
+  TV.print(txtBuf8);
 
   // decision buttons
   }
   TV.set_cursor(wx+4,wy+4);
-  free(txtBuff);
+  
 }
 
-static void drawDesktop(){
+void drawDesktop(){
   
   switch(wallpaper){
     case 0: TV.bitmap(0,0,img_wallpaper0); break;
@@ -234,18 +242,20 @@ static void drawDesktop(){
     break;
   }
   
-  drawIcon(0,0,selectedIcon == 0);
-  drawIcon(1,1,selectedIcon == 1);
-  drawIcon(2,2,selectedIcon == 2);
-  drawIcon(4,3,selectedIcon == 3);
-  drawIcon(5,4,selectedIcon == 4);
-  drawIcon(7,5,selectedIcon == 5);
+  drawIcon(0,0,iconIndex == 0);
+  drawIcon(1,1,iconIndex == 1);
+  drawIcon(2,2,iconIndex == 2);
+  drawIcon(3,3,iconIndex == 3);
+  drawIcon(4,4,iconIndex == 4);
+  drawIcon(5,5,iconIndex == 5);
+  drawIcon(6,6,iconIndex == 6);
+  drawIcon(7,7,iconIndex == 7);
 
   clear_cmd();
   prompt();
 }
 
-static void play_tune(const byte no){
+void play_tune(const byte no){
 
   if (no==TUNE_OS){ // MonciaOS tune
     TV.tone(1000,200);_delay_ms(20);
@@ -272,34 +282,31 @@ static void play_tune(const byte no){
   }
 }
 
-static void appLook(){  
-  char txtBuff2[24];
-  drawWindow(100,40,false);
-  
-  strcpy_P(txtBuff2,TXT_LOOKTITLE);
-  TV.print(txtBuff2);
+void appLook(){  
+  byte wallpaperSave = wallpaper;
+  drawWindow(100,40);
+  strcpy_P(txtBuf,TXT_LOOKTITLE);
+  TV.print(txtBuf);
   TV.set_cursor(20,42);
-  strcpy_P(txtBuff2,TXT_LOOKTITLE2);
-  TV.print(txtBuff2);
+  strcpy_P(txtBuf,TXT_LOOKTITLE2);
+  TV.print(txtBuf);
      
   switch (wallpaper) {
-    case 0: strcpy_P(txtBuff2,TXT_LOOK1); break;
-    case 1: strcpy_P(txtBuff2,TXT_LOOK2); break;
-    case 2: strcpy_P(txtBuff2,TXT_LOOK3); break;
-    case 3: strcpy_P(txtBuff2,TXT_LOOK4); break;
+    case 0: strcpy_P(txtBuf,TXT_LOOK1); break;
+    case 1: strcpy_P(txtBuf,TXT_LOOK2); break;
+    case 2: strcpy_P(txtBuf,TXT_LOOK3); break;
+    case 3: strcpy_P(txtBuf,TXT_LOOK4); break;
   }
   TV.set_cursor(20,50);
-  TV.print(txtBuff2);
+  TV.print(txtBuf);
   TV.set_cursor(20,60);
-  strcpy_P(txtBuff2,TXT_LOOKTIP);
-  TV.print(txtBuff2);
+  strcpy_P(txtBuf,TXT_LOOKTIP);
+  TV.print(txtBuf);
 
   play_tune(TUNE_POS);
   
   while (!keyboard.available()) {}
-  
   while (char c = keyboard.read()) {
-    
       if (c == PS2_LEFTARROW) {       
         if (wallpaper > 0) wallpaper--;       
         TV.tone(523,50);
@@ -310,52 +317,57 @@ static void appLook(){
             
       } else if (c == PS2_DOWNARROW) {  
        
-      } else if (c == PS2_ESC or c == PS2_ENTER) {  
-          break;
+      } else if (c == PS2_ESC) {  
+        wallpaper = wallpaperSave;
+        break;
+      } else if (c == PS2_ENTER) {  
+        dimScreen;
+        drawWindow(40,16);
+        strcpy_P(txtBuf,TXT_DONE);
+        TV.print(txtBuf);
+        break;
       }
 
       TV.set_cursor(20,50);
       switch (wallpaper) {
-        case 0: strcpy_P(txtBuff2,TXT_LOOK1); break;
-        case 1: strcpy_P(txtBuff2,TXT_LOOK2); break;
-        case 2: strcpy_P(txtBuff2,TXT_LOOK3); break;
-        case 3: strcpy_P(txtBuff2,TXT_LOOK4); break;
+        case 0: strcpy_P(txtBuf,TXT_LOOK1); break;
+        case 1: strcpy_P(txtBuf,TXT_LOOK2); break;
+        case 2: strcpy_P(txtBuf,TXT_LOOK3); break;
+        case 3: strcpy_P(txtBuf,TXT_LOOK4); break;
       }
-
-      TV.print(txtBuff2);
+      TV.print(txtBuf);
+      
       _delay_ms(5);
   }
-  free(txtBuff2);
 }
 
-static void activateScreensaver(){
+void activateScreensaver(){
 
+  // sinusoid artist
   byte lx=2 + rand()%126;
   byte ly=2 + rand()%94;
   byte x,y;  
 
-  TV.bitmap(0,0,img_wallpaper0);
+  TV.fill(BLACK);
   
   while (!keyboard.available()) {  
     x = 64 + sin(millis()*0.0077f) * 24 + (sin(millis()*0.017)*40); //40
     y = 48 + cos(millis()*0.0066f) * 24 + (sin(millis()*0.013)*24); //24
-    if (millis() % 10000 == 0){
-      TV.bitmap(0,0,img_wallpaper0);
-    }
-    TV.draw_line(lx,ly,x,y,BLACK);
+    if ((millis()*10) % 50000 == 0) TV.fill(BLACK);
+    TV.draw_line(lx,ly,x,y,WHITE);
     lx = x;
     ly = y;
-    //TV.tone(50+x+y*10,4);
   }
+
+  // starfield
+  
 }
 
-static void splash(){
-  TV.bitmap(0,0,img_title); 
-  while (!keyboard.available()) { }
-  keyboard.read();  
+void splash(){
+  TV.bitmap(0,0,img_title);   
 }
 
-static void dim_screen(){
+void dimScreen(){
   static int x,y;
   for (y=0;y<96;y=y+4){
     TV.draw_row(y,0,128,BLACK);
@@ -365,9 +377,7 @@ static void dim_screen(){
   }
 }
 
-static void p1x(){
-  char txtBuff[24];
-  
+void p1x(){
   TV.clear_screen();
   TV.bitmap(128-85,0,img_kj);
   _delay_ms(40);  
@@ -375,75 +385,103 @@ static void p1x(){
   TV.bitmap(24,24,img_p1x);
   TV.tone(329,75);_delay_ms(30);
   _delay_ms(100);
+  
   TV.select_font(font8x8);
-
-  strcpy_P(txtBuff, TXT_KRZYSZTOF);  TV.print(4,62,txtBuff);
-  strcpy_P(txtBuff, TXT_KRYSTIAN);  TV.print(4,72,txtBuff);
-  strcpy_P(txtBuff, TXT_JANKOWSKI);  TV.print(4,82,txtBuff);
-  TV.tone(392,75);_delay_ms(10);
-  _delay_ms(500);
+  strcpy_P(txtBuf, TXT_KRZYSZTOF);  TV.print(4,62,txtBuf);
+  strcpy_P(txtBuf, TXT_KRYSTIAN);  TV.print(4,72,txtBuf);
+  strcpy_P(txtBuf, TXT_JANKOWSKI);  TV.print(4,82,txtBuf);
   TV.select_font(font4x6);
   
-  free(txtBuff);
-  drawDesktop();
+  TV.tone(392,75);_delay_ms(10);
 }
 
 void processCommand(char *command){
-  char txtBuff[24];
-
+  byte appIndex = 200;
   
-  if (strcmp(command, "free") == 0) {
-    dim_screen();
-    drawWindow(100,14);
-    strcpy_P(txtBuff,TXT_FREERAM);
-    TV.print(txtBuff);
-    TV.print(freeRam());
-    strcpy_P(txtBuff,TXT_BYTES);
-    TV.print(txtBuff);
-    play_tune(TUNE_POS);
-  } else if (strcmp(command, "ss") == 0) {  
-    activateScreensaver();
-    play_tune(TUNE_POS);
-  
-  } else if (strcmp(command, "moncia") == 0) {  
-    play_tune(TUNE_POS);
-    splash();
-        
-  } else if (strcmp(command, "about") == 0) {
-    dim_screen();
-    drawWindow(100,50);
-    TV.set_cursor(35,8);
-    strcpy_P(txtBuff,TXT_MONCIAABOUT);
-    TV.print(txtBuff);TV.print(VERSION);    
-    TV.bitmap(64-8,20,img_p1x);
-    TV.set_cursor(40,47);
-    strcpy_P(txtBuff,TXT_CC);
-    TV.print(txtBuff);
-    play_tune(TUNE_OS);
-    
+  if (strcmp(command, "\0") == 0 ) {
+    appIndex = iconIndex;   
+  }else if (strcmp(command, "free") == 0 ) {
+    appIndex = 0;
+  } else if (strcmp(command, "about") == 0) {  
+    appIndex = 1;
   } else if (strcmp(command, "look") == 0) {
-    dim_screen();
-    appLook();
-    
-    _delay_ms(15);
-    
-    drawWindow(60,14);
-    strcpy_P(txtBuff,TXT_DONE);
-    TV.print(txtBuff);
-    
-  
-  } else if (strcmp(command, "p1x") == 0) {
-    p1x();
-   
-  } else {
-    dim_screen();
-    drawWindow(100,14);
-    strcpy_P(txtBuff,TXT_UNKNOWN);
-    TV.print(txtBuff);
-    play_tune(TUNE_NEG);
+    appIndex = 2;
+  } else if (strcmp(command, "p1x") == 0) {  
+    appIndex = 3;
+  } else if (strcmp(command, "edit") == 0) {  
+    appIndex = 4;
+  } else if (strcmp(command, "files") == 0) {  
+    appIndex = 5;
+  } else if (strcmp(command, "ss") == 0) {  
+    appIndex = 100;
+  } else if (strcmp(command, "moncia") == 0) {  
+    appIndex = 101;
   }
   
-  free(txtBuff);
+  switch (appIndex) {
+    case 0: // FREE
+      dimScreen();
+      drawWindow(100,14);
+      strcpy_P(txtBuf,TXT_FREERAM);
+      TV.print(txtBuf);
+      TV.print(freeRam());
+      strcpy_P(txtBuf,TXT_BYTES);
+      TV.print(txtBuf);
+      play_tune(TUNE_POS);
+      break;
+      
+     case 1: // ABOUT
+      dimScreen();
+      drawWindow(100,50);
+      TV.set_cursor(35,8);
+      strcpy_P(txtBuf,TXT_MONCIAABOUT);
+      TV.print(txtBuf);TV.print(VERSION);    
+      TV.bitmap(64-8,20,img_p1x);
+      TV.set_cursor(40,47);
+      strcpy_P(txtBuf,TXT_CC);
+      TV.print(txtBuf);
+      play_tune(TUNE_OS);   
+      break;  
+      
+     case 2: // LOOK
+      dimScreen();
+      appLook(); 
+      break;  
+      
+     case 3: // P1X
+      p1x();
+      break;  
+      
+     case 4:
+     case 5:
+        dimScreen();
+        drawWindow(100,14);
+        strcpy_P(txtBuf,TXT_DEBUG);
+        TV.print(txtBuf);
+        play_tune(TUNE_NEU);
+      break;
+         
+     case 7:
+     case 100: // SCREEN SAVER
+      play_tune(TUNE_POS);
+      activateScreensaver();
+      break;  
+      
+     case 6:
+     case 101: // MONCIA
+      play_tune(TUNE_POS);
+      splash(); 
+      break;  
+      
+     case 200: // UNKNOWN
+        dimScreen();
+        drawWindow(100,14);
+        strcpy_P(txtBuf,TXT_UNKNOWN);
+        TV.print(txtBuf);
+        play_tune(TUNE_NEG);
+      break;
+  }
+  
   while (!keyboard.available()) {}
   keyboard.read();
 }
@@ -454,15 +492,17 @@ void loop() {
     char c = keyboard.read();
     
     if (c == PS2_ENTER || cmdIndex == CMD_MAX) {
-
       commandBuffer[cmdIndex] = '\0';
       processCommand(commandBuffer);
-      strcpy(commandBufferLast,commandBuffer);
-      cmdIndexLast = cmdIndex;
+      
+      if (cmdIndex > 0){        
+        strcpy(commandBufferLast,commandBuffer);
+        cmdIndexLast = cmdIndex;       
+      }
+      
       drawDesktop();
-        
+      
     } else if (c == PS2_ESC) {
-
       play_tune(TUNE_NEU);
       drawDesktop();
       
@@ -477,14 +517,14 @@ void loop() {
         play_tune(TUNE_NEG);
       }
     } else if (c == PS2_LEFTARROW) {
-      if (selectedIcon>0){ 
-        selectedIcon--;
+      if (iconIndex>0){ 
+        iconIndex--;
         TV.tone(523,50);
         drawDesktop();
       }
     } else if (c == PS2_RIGHTARROW) {
-      if (selectedIcon<5) { 
-        selectedIcon++; 
+      if (iconIndex<ICONS) { 
+        iconIndex++; 
         TV.tone(523,50);
         drawDesktop(); 
       }
